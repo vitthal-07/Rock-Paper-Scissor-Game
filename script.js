ff59925ade88f3f98653625acd844e9fbfb90d62
Vitthal Biradar <124929688+ItsmeVt07@users.noreply.github.com>
2023-12-26 11:23:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 
-const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#comp-score");
+const userScorePara = document.querySelector("#user");
+const compScorePara = document.querySelector("#comp");
 
 const genCompChoice = () => {
   const options = ["rock", "paper", "scissors"];
